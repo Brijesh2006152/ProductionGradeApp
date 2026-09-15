a1f4ea6538ad0866d89de71cbac87d2bc6119334
Brijesh2006152 <brijeshpatel2006152@gmail.com>
2026-09-15 17:04:48 +0530

    utility code added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 
 dotenv.config({
     path: './.env'
@@ -19,6 +22,15 @@ const app = express()
             app.listen(process.env.PORT, () => {
                 console.log(`App is listen at port: ${process.env.PORT}`);
             });
+
+            app.use(cors({
+                origin: process.env.CORS_ORIGIN,
+                credentials: true
+            }))
+            app.use(express.json({limit: "16kb"}))
+            app.use(express.urlencoded({extended: true, limit: "16kb"}))
+            app.use(express.static("public"))
+            app.use(cookieParser)
         } catch (error) {
             console.error("Error: ", error);
             throw error;
